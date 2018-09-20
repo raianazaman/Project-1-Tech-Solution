30e528571040d4e9c54b0617005051603ab6f87c
Quy Nguyen <nguyenqb24@gmail.com>
2018-09-20 17:39:18 -0400

    update

---
+++ javascript/firebase.js
@@ -0,0 +1,11 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBdOgmeJMsbFhc7NoDTOxLycFBuK6NeyZE",
+    authDomain: "unc-coding-project-1.firebaseapp.com",
+    databaseURL: "https://unc-coding-project-1.firebaseio.com",
+    projectId: "unc-coding-project-1",
+    storageBucket: "unc-coding-project-1.appspot.com",
+    messagingSenderId: "387514743330"
+  };
+  firebase.initializeApp(config);
+
