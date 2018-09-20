24c7dc9943d0aa2dac8a0b47559b83598a2b7258
Quy Nguyen <nguyenqb24@gmail.com>
2018-09-20 18:27:26 -0400

    firebase success

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -9,3 +9,44 @@
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
+
+$("#userInfo").on("click", function(event){
+    event.preventDefault();
+
+    var firstName = $("#first_name").val().trim();
+    var lastName = $("#last_name").val().trim();
+    var phoneNumber = $("#phoneNumber").val().trim();
+    var email = $("#email").val().trim();
+  
+
+    console.log(firstName)
+    console.log(lastName)
+    console.log (phoneNumber)
+    console.log(email)
+
+    if ( firstName && lastName && phoneNumber && email){
+
+        $("#first_name").val("");
+        $("#last_name").val("");
+        $("#phoneNumber").val("");
+        $("#email").val("");
+
+        database.ref().push({
+            firstName: firstName,
+            lastName: lastName,
+            phoneNumber: phoneNumber,
+            email: email
+            
+        });
+        }   
+    
+        else{
+            console.log("empty form");
+        }
+
+    });
+
+
+
+    
